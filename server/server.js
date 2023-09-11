faa4a66e5f957883531fba8e8b8368b2fd3e6ab4
Sagar Mainali <sagarmainali78@gmail.com>
2023-09-11 17:49:46 +0545

    connected backend and frontend and added loader component @frontend

--- server/server.js
+++ server/server.js
@@ -1,12 +1,34 @@
 import express from 'express';
-// import mysql from 'mysql2';
+import cors from 'cors'
+import mysql from 'mysql2';
 
 const app = express();
 
+// using middleware
+app.use(cors());
+app.use(express.json());
+
+// establishing connection to remote database
+const database = mysql.createPool({
+    host: 'localhost',
+    port: '3307',
+    user: 'root',
+    password: 'mysql@wb_2023',
+    database: 'documents'
+})
+
+//default route 
 app.get('/', (req, res) => {
     res.send("Hello World!");
 })
 
+app.post('/api/documents/', (req, res) => {
+    const data = req.body;
+    console.log(data);
+    res.send(data)
+})
+
+// dynamic assignment of port number
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => console.log(`The server is running at ${port}...`));
