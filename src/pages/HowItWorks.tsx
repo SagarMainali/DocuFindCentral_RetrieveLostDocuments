0d67f2e9fb9f4193bd361bced08a307fa0ceb68f
Sagar Mainali <sagarmainali78@gmail.com>
2023-09-28 20:09:14 +0545

    using replacements for components props in Trans component

--- src/pages/HowItWorks.tsx
+++ src/pages/HowItWorks.tsx
@@ -1,10 +1,14 @@
 import { useAppSelector } from "../redux/hooks"
-import { useTranslation } from 'react-i18next'
+import { useTranslation, Trans } from 'react-i18next'
 
 export default function HowItWorks() {
 
   const isLight = useAppSelector((state) => state.navbar.isLight)
   const { t } = useTranslation('howItWorks_ns')
+
+  const replacements = {
+    b: <b />
+  }
 
   return (
     <div className="cstm-paged justify-start">
@@ -12,15 +16,15 @@ export default function HowItWorks() {
       <div className="w-full">
         <h1 className={`mb-8 ${isLight ? 'title-light' : 'title-dark'}`}>How it works</h1>
 
-        <ul className="flex flex-col gap-3 text-[14px] mb-8">
+        <ul className="flex flex-col gap-3 mb-8">
           <li>{t('1')}</li>
-          <li>{t('2')}</li>
+          <li><Trans ns="howItWorks_ns" components={replacements}>2</Trans></li>
           <li>{t('3')}</li>
-          <li>{t('4')}</li>
-          <li>{t('5')}</li>
+          <li><Trans ns="howItWorks_ns" components={replacements}>4</Trans></li>
+          <li><Trans ns="howItWorks_ns" components={replacements}>5</Trans></li>
           <li>{t('6')}</li>
         </ul>
-        <p className="note">{t('note')}</p>
+        <p><i><Trans ns="howItWorks_ns" components={replacements}>note</Trans></i></p>
       </div>
 
     </div>
