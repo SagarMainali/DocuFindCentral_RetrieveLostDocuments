a09cb2d3d53219fc36de54288e29faf868646ff4
Sagar Mainali <sagarmainali78@gmail.com>
2024-05-02 09:40:50 +0545

    added a new input field for driving license document type along with license issued place options

---
+++ client/src/options/licenseIssuedPlace.ts
@@ -0,0 +1,18 @@
+const licenseIssuedPlace = [
+    { label: "Bagmati", value: "Bagmati" },
+    { label: "Gandaki", value: "Gandaki" },
+    { label: "Lumbini", value: "Lumbini" },
+    { label: "Narayani", value: "Narayani" },
+    { label: "Dhaulagiri", value: "Dhaulagiri" },
+    { label: "Janakpur", value: "Janakpur" },
+    { label: "Rapti", value: "Rapti" },
+    { label: "Karnali", value: "Karnali" },
+    { label: "Bheri", value: "Bheri" },
+    { label: "Seti", value: "Seti" },
+    { label: "Mahakali", value: "Mahakali" },
+    { label: "Sagarmatha", value: "Sagarmatha" },
+    { label: "Koshi", value: "Koshi" },
+    { label: "Mechi", value: "Mechi" }
+]
+
+export default licenseIssuedPlace
